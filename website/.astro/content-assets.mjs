796908fb6c6acda1718f0c192422cf5392019346
roll <eskarev@gmail.com>
2026-02-12 10:37:35 +0000

    Add python link

---
+++ website/.astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_1rjHIf from "./assets/jupyter.png?astroContentImageFlag=&importer=content%2Fdocs%2Ftypescript%2Fjupyter.md";
+export default new Map([["./assets/jupyter.png?astroContentImageFlag=&importer=content%2Fdocs%2Ftypescript%2Fjupyter.md", __ASTRO_IMAGE_IMPORT_1rjHIf]]);
+		
